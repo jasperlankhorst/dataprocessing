3fa1972d3dd0ad3acaeed08d52447953546eaf47
jasperlankhorst <lankhorstjasper@gmail.com>
2018-12-05 23:25:39 +0100

    Scatterplot, nog lang niet af

--- homework/week_5/scatter.js
+++ homework/week_5/scatter.js
@@ -8,10 +8,36 @@ window.onload = function() {
 };
 
 window.onload = function() {
-  var womenInScience = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015";
-  var consConf = "https://data.mprog.nl/course/10%20Homework/100%20D3%20Scatterplot/datasets/consconf.json";
-  var requests = [d3.json(womenInScience), d3.json(consConf)];
+  $.getJSON("https://data.mprog.nl/course/10%20Homework/100%20D3%20Scatterplot/datasets/msti.json",
+    function(data) {
 
-  Promise.all(requests).then(function(response) {
-    console.log(d3.json(consConf));
-})}
+
+// svg aanmaken
+
+      var svg = d3.select("body").append("svg")
+                                 .attr("width", 200)
+                                 .attr("height", 200);
+
+// cirkels
+
+      var circles = svg.selectAll("circle")
+                     .data(root)
+                     .enter()
+                     .append("circle")
+                     .attr("y", function(root) {
+                       var y = 
+                       return y
+                     })
+                     // .attr("width", 10)
+                     // .attr("height", function(root) {
+                     //   return root["value"];
+                     // })
+                     // .attr("fill", "rgb(200,200,200)")
+                     // .attr("transform", function(root,i) {
+                     //   var translate = [];
+                     //   return "translate(" + translate + ")";
+                     });
+         });
+    });
+
+};

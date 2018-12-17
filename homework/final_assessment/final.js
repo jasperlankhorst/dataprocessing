f4fd079b138c04f94eeffbab1f25ae19ccd0c495
jasperlankhorst <lankhorstjasper@gmail.com>
2018-12-17 23:29:16 +0100

    wegens heuristieken niet verder dan dit gekomen

---
+++ homework/final_assessment/final.js
@@ -0,0 +1,14 @@
+/*
+This script combines a bar chart and a pie chart for the course Data Processing.
+
+Jasper Lankhorst
+10885803
+
+Sources:
+http://bl.ocks.org/WilliamQLiu/76ae20060e19bf42d774
+*/
+
+window.onload = function() {
+  d3.queue()
+    .
+}
